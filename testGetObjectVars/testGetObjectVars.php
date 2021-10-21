5a4b7efba1488771768f3b85350833bc0b31c6cd
Alexey Vasilyev <you@example.com>
2021-10-21 23:07:17 +0300

    updated for export

--- testGetObjectVars/testGetObjectVars.php
+++ testGetObjectVars/testGetObjectVars.php
@@ -24,18 +24,20 @@ foreach( instance_to_array($object) as $key =>$value ) {
 	echo "$key  == $value\n";
 }
 
-echo "\n\nimport from json with simple reflection:\n";
+echo "\nimport from json with simple reflection:\n";
 $jsonArray = json_decode('{"name":"text", "value":10}', true );
 foreach($jsonArray as $key  => $value ) {
 	//my own reflection
 	$object->setPropertyValue((string)$key, $value );
+	echo "prop $key: " . $object->getPropertyValue( (string)$key ) ."\n";
 }
 
-
-echo "\n\nget value via reflection:\n";
-echo "prop value: " . $object->getPropertyValue( "name") ."\n";
 
 echo "instance_to_array with updated object:\n";
+$array = [];
 foreach( instance_to_array($object) as $key =>$value ) {
 	echo "$key  == $value\n";
+	$array[$key] = $value;
 }
+
+echo json_encode($array);
